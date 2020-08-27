e5d056ed23b4b9ac3eb28fb320cfe5009b41abb9
Sisi55 <ksisi1755@gmail.com>
2020-08-27 13:40:05 +0900

    Add static collect

--- config/settings.py
+++ config/settings.py
@@ -122,10 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-STATIC_ROOT = '/var/www/static'#os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 # cors
 CORS_ORIGIN_ALLOW_ALL = True
