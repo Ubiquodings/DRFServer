6322b9b8d4014753508b7bc2bcad5c1834547c6a
Sisi55 <ksisi1755@gmail.com>
2020-08-27 13:33:29 +0900

    Add settings

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/static'#os.path.join(BASE_DIR, 'static')
 
 # cors
 CORS_ORIGIN_ALLOW_ALL = True
