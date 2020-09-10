09c89fa1eab9d3e387285508806b9678c8bebe49
Sisi55 <ksisi1755@gmail.com>
2020-09-10 19:22:10 +0900

    Add 추천 service 로직 {스부 > 장고 > ES} 로 변경

--- cf_app/urls.py
+++ cf_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     # /cf/
     path('test/', views.test_api),
+
+    # index_name & action_type 받아서 파싱 후 결과 반환하는 api
     path('es-list/', views.test_es_list_api),
+
+    # index_name & user_id 받아서 {카테고리:점수} 반환하는 api
+    path('category-score/', views.get_last50_for_category_score_from_ES),
+
     path('es-index-list/', views.test_es_get_all_api),
 ]
--- cf_app/views.py
+++ cf_app/views.py
@@ -1,3 +1,5 @@
+import collections
+
 import urllib3
 import json
 import pandas as pd
@@ -19,6 +21,9 @@ def test_api(request):
 
 @api_view(['GET'])
 def test_es_list_api(request):
+    """
+    http://127.0.0.1:8000/cf/es-list/?index_name=ubic_click_action&action_type=order
+    """
     index_name = request.query_params.get('index_name')
     actionType = request.query_params.get('action_type')
 
@@ -45,14 +50,8 @@ def test_es_list_api(request):
         filter_path=['hits.hits._source']  # 이 아래 내용만 나온다는건데 별로 필요없음
     )
     # print(response)
-    response = response['hits']['hits']  # list : filter_path
-    # df = pd.DataFrame(response)
 
-    # response 루프돌면서 _source 의 값들의 배열로 만들거야
-    result = []
-    for row in response:
-        row = row['_source']
-        result.append(row)
+    result = get_pretty_response(response)
 
     ''' 결과 배열의 원소 모양 : 자바에서 바로 클래스 배열로 받으면 된다
         {
@@ -64,6 +63,73 @@ def test_es_list_api(request):
         },
     '''
     return Response(result, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])  # userId 가 필요하네 !
+def get_last50_for_category_score_from_ES(request):
+    """
+        엘라스틱서치에서 최근 50개 행동 가져와서 category_score 로 변경하는 함수
+        http://127.0.0.1:8000/cf/category-score/?index_name=ubic_click_action&user_id=47161
+    """
+    index_name = request.query_params.get('index_name')
+    user_id = request.query_params.get('user_id')
+
+    client = Elasticsearch(settings.ES_SECRET_KEY)
+
+    size = client.count(
+        index=index_name,
+        body={'query': {"match": {
+            "userId": user_id
+        }}}  # _all
+    )["count"]
+    print(size)
+
+    response = client.search(
+        index=index_name,
+        body={
+            "query": {
+                "match": {  # _all
+                    "userId": user_id
+                }
+            }
+        },
+        size=size,
+        filter_path=['hits.hits._source']  # 이 아래 내용만 나온다는건데 별로 필요없음
+    )
+    # print(response)
+
+    # category_score 계산 로직
+    response = get_pretty_response(response)
+    category_score = collections.defaultdict(int)
+    for action in response:
+        category_score[action['categoryId']] += 1
+
+    # 근데 사실 필요한건 가장 점수가 높은 카테고리 값!
+    def return_value_from_dict(x):
+        return x[1]
+
+    # print('정렬전 ', category_score)
+    result = sorted(category_score.items(), key=return_value_from_dict, reverse=True)
+    print('정렬후 ', result)
+    # result[0][0] # category:score 니까 [0]
+
+    # result = []
+    return Response({
+        'maxScoreCategory': result[0][0]
+    }, status=status.HTTP_200_OK)
+
+
+def get_pretty_response(response):
+    response = response['hits']['hits']  # list : filter_path
+    # df = pd.DataFrame(response)
+
+    # response 루프돌면서 _source 의 값들의 배열로 만들거야
+    result = []
+    for row in response:
+        row = row['_source']
+        result.append(row)
+
+    return result
 
 
 # def search_test():
